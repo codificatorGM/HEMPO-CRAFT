0fcb46bd340a491f11fb1bc6ce0d0f8d32c45c6c
CodificatorGM <hruns2000@gmail.com>
2023-09-21 16:09:16 -0600

    Updated file and fixed errors in gradle

--- src/main/java/net/codificatorgm/testmod/TestMod.java
+++ src/main/java/net/codificatorgm/testmod/TestMod.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+// The value here should match an entry in the META-INF/mods.toml files
 @Mod(TestMod.MOD_ID)
 public class TestMod {
     // Define mod id in a common place for everything to reference

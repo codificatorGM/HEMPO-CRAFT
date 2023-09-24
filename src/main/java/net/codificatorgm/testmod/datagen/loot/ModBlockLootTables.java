78f3700b11921dfda20ba06432b5f6e7e8e958b5
CodificatorGM <hruns2000@gmail.com>
2023-09-24 15:47:46 -0600

    Added Hemp Blocks and variants(stairs, button, trapdoor, etc)...

--- src/main/java/net/codificatorgm/testmod/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/codificatorgm/testmod/datagen/loot/ModBlockLootTables.java
@@ -18,6 +18,7 @@ import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
@@ -26,24 +27,42 @@ public class ModBlockLootTables extends BlockLootSubProvider {
     public ModBlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
+
     @Override
     protected void generate() {
 
-        this.add(ModBlocks.WEED_BLOCK.get(),
-                block -> createLikeSilkTouchOrShearsDispatchTable(ModBlocks.WEED_BLOCK.get()
-                ,LootItem.lootTableItem(ModItems.WEED_LEAF.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 3.0F)))));
+        this.add(ModBlocks.WEED_LEAVES.get(),
+                block -> createLikeSilkTouchOrShearsDispatchTable(ModBlocks.WEED_LEAVES.get()
+                        , LootItem.lootTableItem(ModItems.WEED_LEAF.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 3.0F)))));
 
         LootItemCondition.Builder lootitemcondition$builder = LootItemBlockStatePropertyCondition
                 .hasBlockStateProperties(ModBlocks.WEED_CROP.get())
                 .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(WeedCropBlock.AGE, 5));
-
-            this.add(ModBlocks.WEED_CROP.get(), createCropDrops(ModBlocks.WEED_CROP.get(), ModItems.HARVESTED_WEED.get() ,
+        this.add(ModBlocks.WEED_CROP.get(), createCropDrops(ModBlocks.WEED_CROP.get(), ModItems.HARVESTED_WEED.get(),
                 ModItems.WEED_SEEDS.get(), lootitemcondition$builder));
 
+        this.dropSelf(ModBlocks.HEMP_BLOCK.get());
+        this.dropSelf(ModBlocks.HEMP_FENCE.get());
+        this.dropSelf(ModBlocks.HEMP_STAIRS.get());
+        this.dropSelf(ModBlocks.HEMP_BUTTON.get());
+        this.dropSelf(ModBlocks.HEMP_FENCE.get());
+        this.dropSelf(ModBlocks.HEMP_FENCE_GATE.get());
+        this.dropSelf(ModBlocks.HEMP_PRESSURE_PLATE.get());
+        this.dropSelf(ModBlocks.HEMP_TRAPDOOR.get());
+
+        this.add(ModBlocks.HEMP_SLAB.get(),
+                block -> createSlabItemTable(ModBlocks.HEMP_SLAB.get()));
+        this.add(ModBlocks.HEMP_DOOR.get(),
+                block -> createDoorTable(ModBlocks.HEMP_DOOR.get()));
+
+
+
     }
+
     protected LootTable.Builder createCropDropsLike(Block pCropBlock, Item pGrownCropItem, Item pSeedsItem, LootItemCondition.Builder pDropGrownCropCondition) {
         return this.applyExplosionDecay(pCropBlock, LootTable.lootTable().withPool(LootPool.lootPool().add(LootItem.lootTableItem(pGrownCropItem).when(pDropGrownCropCondition).otherwise(LootItem.lootTableItem(pSeedsItem)))).withPool(LootPool.lootPool().when(pDropGrownCropCondition).add(LootItem.lootTableItem(pSeedsItem).apply(ApplyBonusCount.addBonusBinomialDistributionCount(Enchantments.BLOCK_FORTUNE, 0.5714286F, 3)))));
     }
+
     protected static LootTable.Builder createLikeSilkTouchOrShearsDispatchTable(Block pBlock, LootPoolEntryContainer.Builder<?> pBuilder) {
         return createSelfDropDispatchTable(pBlock, HAS_SHEARS.or(HAS_SILK_TOUCH), pBuilder);
     }

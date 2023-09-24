a4a8bd5bc5a2135fd63a7a0926dc79e69aeab8b9
CodificatorGM <hruns2000@gmail.com>
2023-09-24 15:49:16 -0600

    Added Hemp Blocks and variants(stairs, button, trapdoor, etc)...

--- src/main/java/net/codificatorgm/testmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/codificatorgm/testmod/datagen/ModItemModelProvider.java
@@ -1,13 +1,16 @@
 package net.codificatorgm.testmod.datagen;
 
 import net.codificatorgm.testmod.TestMod;
+import net.codificatorgm.testmod.block.ModBlocks;
 import net.codificatorgm.testmod.item.ModItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItemModelProvider extends ItemModelProvider {
@@ -23,11 +26,49 @@ public class ModItemModelProvider extends ItemModelProvider {
         simpleItem(ModItems.WEED_BUD);
         simpleItem(ModItems.WEED_SEEDS);
         simpleItem(ModItems.HARVESTED_WEED);
+
+        simpleBlockItem(ModBlocks.HEMP_DOOR);
+
+        fenceItem(ModBlocks.HEMP_FENCE, ModBlocks.HEMP_BLOCK);
+        buttonItem(ModBlocks.HEMP_BUTTON, ModBlocks.HEMP_BLOCK);
+
+        evenSimplerBlockItem(ModBlocks.HEMP_STAIRS);
+        evenSimplerBlockItem(ModBlocks.HEMP_SLAB);
+        evenSimplerBlockItem(ModBlocks.HEMP_PRESSURE_PLATE);
+        evenSimplerBlockItem(ModBlocks.HEMP_FENCE_GATE);
+
+        trapdoorItem(ModBlocks.HEMP_TRAPDOOR);
     }
 
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(TestMod.MOD_ID,"item/" + item.getId().getPath()));
+                new ResourceLocation(TestMod.MOD_ID, "item/" + item.getId().getPath()));
+    }
+
+    public void evenSimplerBlockItem(RegistryObject<Block> block) {
+        this.withExistingParent(TestMod.MOD_ID + ":" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
+                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath()));
+    }
+
+    public void trapdoorItem(RegistryObject<Block> block) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
+                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath() + "_bottom"));
+    }
+
+    public void fenceItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/fence_inventory"))
+                .texture("texture", new ResourceLocation(TestMod.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    public void buttonItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/button_inventory"))
+                .texture("texture", new ResourceLocation(TestMod.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    private ItemModelBuilder simpleBlockItem(RegistryObject<Block> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(TestMod.MOD_ID, "item/" + item.getId().getPath()));
     }
 }

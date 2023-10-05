0f27d446d23c57d5278dad700473e1b3e8664d41
CodificatorGM <hruns2000@gmail.com>
2023-10-05 10:47:36 -0600

    Added JEI COMPABILITY (MUST HAVE JEI INSTALLED)

--- src/main/java/net/codificatorgm/WeedOCraft/datagen/DataGenerators.java
+++ src/main/java/net/codificatorgm/WeedOCraft/datagen/DataGenerators.java
@@ -1,6 +1,6 @@
-package net.codificatorgm.testmod.datagen;
+package net.codificatorgm.WeedOCraft.datagen;
 
-import net.codificatorgm.testmod.TestMod;
+import net.codificatorgm.WeedOCraft.TestMod;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
